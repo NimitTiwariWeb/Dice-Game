3de07980175669a12894ec402bac72b7b7838049
Nimit Tiwari <nimittiwari2604@gmail.com>
2022-10-30 21:41:34 +0530

    starting condition of the game3

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting Element
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting Condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+  // Hiding dice
+diceEl.classList.add('hidden');
